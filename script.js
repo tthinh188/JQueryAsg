e00426e4558e1948d25bfa8582cd35f0cafaa20c
Thinh Phan <thinh.phan.18@cnu.edu>
2021-05-28 16:52:00 -0400

    update

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let people;
-let index = -1;
-let limit = 10;
+const readJsonFile = (() => {
+    const getAllStudents = (fileName) => (
+        $.getJSON(fileName)
+    )
+    return { getAllStudents }
+})();
 
-const fetchData = async (fileName) => {
-    await $.getJSON(fileName).then(data => people = data);
-    showRecord(people, limit);
-}
-fetchData('jqueryassignmentdummydata.json');
+const View = (() => {
+    // dom keywords;
+    const domString = {
+        table: 'table',
+        editForm: 'edit_form',
+        searchForm: 'search_form',
+    }
 
-const showRecord = async (records, numOfRecords) => {
-    $('table').find("tr:gt(1)").remove()
-    let listOfRecord = records.slice(0, numOfRecords)
-    await listOfRecord.map((person, i) => {
-        $('table').append(
-            `<tr id=row${i}>
+    // build tmp;
+    const showRecords = (records, numOfRecords) => {
+        $(domString.table).find("tr:gt(1)").remove()
+
+        let listOfRecord = records.slice(0, numOfRecords)
+        listOfRecord.map((person, i) => {
+            $('table').append(
+                `<tr id=row${i}>
                     <td>${person.firstname}</td>
                     <td>${person.lastname}</td>
                     <td>${person.email}</td>
@@ -28,34 +35,168 @@ const showRecord = async (records, numOfRecords) => {
                     <td><button onclick='showForm(${i})' class='primary_btn' id='edit_btn' >Edit</button></td>
                     <td><button onclick='handleDelete(${i})' class='secondary_btn' id='delete_btn'>Delete</button></td>
                 </tr>`
-        )
-    });
+            )
+        });
+        localStorage.setItem('students', JSON.stringify(listOfRecord))
+    }
 
-    localStorage.setItem('students', JSON.stringify(listOfRecord))
+    return {
+        domString,
+        showRecords
+    };
+})();
+
+
+const Module = ((api, view) => {
+    const getAllStudents = api.getAllStudents;
+
+    class State {
+        #studentList = [];
+        #numOfRecords = 10;
+        #index = -1;
+
+        get numOfRecords() {
+            return this.#numOfRecords;
+        }
+
+        set numOfRecords(num) {
+            this.#numOfRecords = num;
+        }
+
+        get studentList() {
+            return this.#studentList;
+        }
+
+        set studentList(arr) {
+            this.#studentList = arr;
+            view.showRecords(this.#studentList, this.#numOfRecords);
+        }
+
+        get studentIndex() {
+            return this.#index;
+        }
+
+        set studentIndex(num) {
+            this.#index = num;
+        }
+    }
+
+    return {
+        getAllStudents,
+        State
+    }
+})(readJsonFile, View)
+
+const Controller = ((view, module) => {
+    const state = new module.State();
+    const addListenerElements = () => {
+        $('#submit').on('click', () => {
+            state.numOfRecords = parseInt($('#limit').val());
+            view.showRecords(state.studentList, state.numOfRecords);
+            windowScroll();
+        })
+
+        $('.edit_form').submit((e) => {
+            e.preventDefault();
+            const location = $('#location').val().split(',');
+
+            const student = {
+                firstname: $('#firstName').val(),
+                lastname: $('#lastName').val(),
+                email: $('#email').val(),
+                location: location,
+                phone: $('#phone').val(),
+                address: {
+                    communication: $('#communication').val(),
+                    permanent: $('#permanent').val()
+                },
+                marks: {
+                    english: $('#english').val(),
+                    science: $('#science').val(),
+                    computers: $('#computers').val(),
+                    hardware: $('#hardware').val(),
+                }
+            }
+            if (state.studentIndex >= 0) {
+                state.studentList[state.studentIndex] = student;
+            }
+            else {
+                state.studentList.unshift(student);
+            }
+            closeEditForm();
+            state.studentIndex = -1;
+            View.showRecords(state.studentList, state.numOfRecords)
+        })
+        $('.search_form').submit((e) => {
+            e.preventDefault();
+            const option = $('#searchOption').val();
+            const searchTerm = $('#search_term').val();
+            let filteredStudent = state.studentList;
+            switch (option) {
+                case 'firstname':
+                    filteredStudent = filteredStudent.filter((student) => student.firstname.toLowerCase() === searchTerm.toLowerCase())
+                    View.showRecords(filteredStudent, state.numOfRecords);
+                    break;
+                case 'lastname':
+                    filteredStudent = filteredStudent.filter((student) => student.lastname.toLowerCase() === searchTerm.toLowerCase())
+                    View.showRecords(filteredStudent, state.numOfRecords);
+                    break;
+                case 'location':
+                    filteredStudent = filteredStudent.filter((student) => student.location.includes(searchTerm))
+                    View.showRecords(filteredStudent, state.numOfRecords);
+                    break;
+                case 'phone':
+                    filteredStudent = filteredStudent.filter((student) => student.phone === searchTerm)
+                    View.showRecords(filteredStudent, state.numOfRecords);
+                    break;
+                default:
+                    break;
+            }
+        })
+        windowScroll();
+
+    }
+
+    const init = async () => {
+        await module.getAllStudents('jqueryassignmentdummydata.json').then(data => {
+            state.studentList = data;
+        })
+        addListenerElements();
+
+    }
+
+    return {
+        init,
+        state,
+    };
+})(View, Module);
+
+const handleDelete = (index) => {
+    const students = Controller.state.studentList;
+    students.splice(index, 1)
+    View.showRecords(students, Controller.state.numOfRecords);
 }
 
 const showForm = (i) => {
+    const students = Controller.state.studentList;
     $('.edit_form').addClass('show');
     if (i >= 0) {
-        index = i;
-        $('#firstName').val(people[i].firstname)
-        $('#lastName').val(people[i].lastname)
-        $('#email').val(people[i].email)
-        $('#location').val(people[i].location)
-        $('#phone').val(people[i].phone)
-        $('#communication').val(people[i].address.communication)
-        $('#permanent').val(people[i].address.permanent)
-        $('#english').val(people[i].marks.english)
-        $('#science').val(people[i].marks.science)
-        $('#computers').val(people[i].marks.computers)
-        $('#hardware').val(people[i].marks.hardware)
+        Controller.state.studentIndex = i
+        $('#firstName').val(students[i].firstname)
+        $('#lastName').val(students[i].lastname)
+        $('#email').val(students[i].email)
+        $('#location').val(students[i].location)
+        $('#phone').val(students[i].phone)
+        $('#communication').val(students[i].address.communication)
+        $('#permanent').val(students[i].address.permanent)
+        $('#english').val(students[i].marks.english)
+        $('#science').val(students[i].marks.science)
+        $('#computers').val(students[i].marks.computers)
+        $('#hardware').val(students[i].marks.hardware)
     }
 }
 
-const handleDelete = (index) => {
-    people.splice(index, 1)
-    showRecord(people, limit);
-}
+
 
 const closeEditForm = () => {
     index = -1;
@@ -73,84 +214,18 @@ const closeEditForm = () => {
     $('.edit_form').removeClass('show');
 }
 
-$('#submit').on('click', () => {
-    limit = parseInt($('#limit').val());
-    showRecord(people, limit);
-    windowScroll();
-})
-
-$('.edit_form').submit((e) => {
-    e.preventDefault();
-    const location = $('#location').val().split(',');
-
-    const student = {
-        firstname: $('#firstName').val(),
-        lastname: $('#lastName').val(),
-        email: $('#email').val(),
-        location: location,
-        phone: $('#phone').val(),
-        address: {
-            communication: $('#communication').val(),
-            permanent: $('#permanent').val()
-        },
-        marks: {
-            english: $('#english').val(),
-            science: $('#science').val(),
-            computers: $('#computers').val(),
-            hardware: $('#hardware').val(),
-        }
-    }
-    if (index >= 0) {
-        people[index] = student;
-    }
-    else {
-        people.push(student);
-    }
-    closeEditForm();
-    index = -1;
-    showRecord(people, limit)
-})
-
-$('.search_form').submit((e) => {
-    e.preventDefault();
-    const option = $('#searchOption').val();
-    const searchTerm = $('#search_term').val();
-    let filteredStudent = people;
-    switch (option) {
-        case 'firstname':
-            filteredStudent = filteredStudent.filter((student) => student.firstname.toLowerCase() === searchTerm.toLowerCase())
-            showRecord(filteredStudent);
-            break;
-        case 'lastname':
-            filteredStudent = filteredStudent.filter((student) => student.lastname.toLowerCase() === searchTerm.toLowerCase())
-            showRecord(filteredStudent)
-            break;
-        case 'location':
-            filteredStudent = filteredStudent.filter((student) => student.location.includes(searchTerm))
-            showRecord(filteredStudent)
-            break;
-        case 'phone':
-            filteredStudent = filteredStudent.filter((student) => student.phone === searchTerm)
-            showRecord(filteredStudent)
-            break;
-        default:
-            break;
-    }
-})
-
 const windowScroll = () => {
     $(window).on('scroll', () => {
         if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-            limit += 10;
-            let copy = people.slice(0, limit);
-            showRecord(copy, limit);
+            Controller.state.numOfRecords += 10;
+            let students = Controller.state.studentList.slice(0, Controller.state.numOfRecords);
+            View.showRecords(students, Controller.state.numOfRecords);
 
-            if (limit > people.length) {
+            if (Controller.state.numOfRecords > Controller.state.studentList.length) {
                 $('table').append(`<tr><th colspan="13">No more record to show</th></tr>`)
                 $(window).off('scroll');
             }
         }
     });
 }
-
-windowScroll();
+Controller.init();
